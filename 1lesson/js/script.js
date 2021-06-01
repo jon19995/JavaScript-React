832bf409f2065c3b8fe61dfaa269046f95dd808f
Evgenii <jon-95@list.ru>
2021-06-01 10:19:44 +0300

    first commit

---
+++ 1lesson/js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const answers = [];
+
+// answers[0] = prompt("Как вас зовут?", "");
+// answers[1] = prompt("ваш возраст?", "");
+// answers[2] = prompt("расскажите о себе?", "");
+
+console.log(typeof(answers));
